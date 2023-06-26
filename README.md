1fce228e3ef02e54b02262bb35c006bf4ea6eaaf
Willem <willemsporen@gmail.com>
2023-06-26 22:39:18 +0200

    It works

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-## DankTimesBot RemindMe! plugin.
-
-### Usage
-Any text that contains `@DankTimesBot RemindMe! [number] [hours|minutes|seconds] [Shoutout]` is parsed as a new reminder.
-For example:
-
-```
-> Hey @DankTimesBot RemindMe! 2 hours Do the dishes
-```
+## DankTimesBot RemindMe plugin.
--- plugin.ts
+++ plugin.ts
@@ -1,77 +1,214 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import TelegramBot from "node-telegram-bot-api";
+import { BotCommand } from "../../src/bot-commands/bot-command";
+import { Chat } from "../../src/chat/chat";
+import { User } from "../../src/chat/user/user";
+import { ChatResetEventArguments } from "../../src/plugin-host/plugin-events/event-arguments/chat-reset-event-arguments";
+import { EmptyEventArguments } from "../../src/plugin-host/plugin-events/event-arguments/empty-event-arguments";
+import { PluginEvent } from "../../src/plugin-host/plugin-events/plugin-event-types";
 import { AbstractPlugin } from "../../src/plugin-host/plugin/plugin";
-import { PLUGIN_EVENT } from "../../src/plugin-host/plugin-events/plugin-event-types"
-import { UserScoreChangedPluginEventArguments } from "../../src/plugin-host/plugin-events/event-arguments/user-score-changed-plugin-event-arguments";
-import { PrePostMessagePluginEventArguments } from "../../src/plugin-host/plugin-events/event-arguments/pre-post-message-plugin-event-arguments";
-import { LeaderboardResetPluginEventArguments } from "../../src/plugin-host/plugin-events/event-arguments/leaderboard-reset-plugin-event-arguments";
-import { TimerTickPluginEventArguments } from "../../src/plugin-host/plugin-events/event-arguments/timer-tick-plugin-event-arguments";
 
-class Reminder
-{
-  readonly Initiated: Date;
-  readonly Trigger: Date;
-  readonly Reason: string;
+interface RemindMeStateCache {
+    chatId: number;
+    reminders: RemindMeReminder[];
+}
 
-  constructor(_time: number, _reason: string)
-  {
-    this.Initiated = new Date();
-    this.Trigger = new Date();
-    this.Trigger.setTime(this.Trigger.getTime() + 1000);
-    console.log("Set a new trigger for: " + this.Trigger);
-    this.Reason = _reason;
-  }
+interface RemindMeReminder {
+    user: number;
+    reason: string;
+    reminded: boolean;
+    remindUserAt: Date;
+    messageId: number;
 }
 
 /**
- * Danktimes RemindMe!
+ * Example of the simplest DankTimesBot
+ * plugin. Can be used as a template to
+ * build new plugins.
  */
-export class Plugin extends AbstractPlugin
-{
-  readonly Pattern: RegExp = /^.*\@DankTimesBot RemindMe! (\d+)\s(hours)(.*)$/;
-  Reminders: Reminder[] = [];
+export class Plugin extends AbstractPlugin {
 
-  /**
-   * A plugin should call its base constructor to
-   * provide it with an identifier, a version
-   * and some optional data.
-   */
-  constructor()
-  {
-    super("RemindMe!", "1.0.0", {});
+    private static readonly REMIND_ME_DATA_FILE = "remind-me-data.json";
 
-    this.subscribeToPluginEvent(PLUGIN_EVENT.PLUGIN_EVENT_TIMER_TICK, (_data: TimerTickPluginEventArguments) => {
-      let output: string[] = [];
-      let expired: Reminder[] = [];
+    private static readonly qtyUnitPattern = /^\/remindme\s+(?<qty>-?\d+)\s+(?<unit>\w+)\s+(?<reason>.+)$/mi;
+    private static readonly hourMinutePattern = /^\/remindme\s+(?<hour>\d{2}):(?<minute>\d{2})\s+(?<reason>.+)$/mi;
 
-      this.Reminders.forEach(reminder => {
-        if((new Date()) > reminder.Trigger)
-          {
-            output.push("RememberMe? " + reminder.Reason);
-            expired.push(reminder);
-          }
-      });
+    private remindMeData: { [chatId: number]: RemindMeStateCache } = {};
 
-      console.log(expired);
-      expired.forEach(_exp => this.Reminders.slice(this.Reminders.indexOf(_exp), 1));
-      return output;
-    });
+    private static _sCheckInProgress = false;
+    private static _runningChecks = false;
 
-    this.subscribeToPluginEvent(PLUGIN_EVENT.PLUGIN_EVENT_PRE_MESSAGE, (_data: PrePostMessagePluginEventArguments) =>
-    {
-      if(this.Pattern.test(_data.Message))
-      {
-        let match: RegExpExecArray = this.Pattern.exec(_data.Message);
-        let hours: number = +match[1];
-        if(hours < 1) return;
+    constructor() {
+        super("RemindMe Plugin", "1.0.0");
 
-        let format: string = match[2];
-        if(format.toUpperCase() !== "HOURS") return;
+        this.subscribeToPluginEvent(PluginEvent.BotStartup, this.onBotStartup.bind(this));
 
-        let reason: string = match[3];
-        if(reason.length === 0) return;
+        this.subscribeToPluginEvent(PluginEvent.ChatReset, this.clearRemindersForChat.bind(this));
 
-        this.Reminders.push(new Reminder(hours, reason));
-        return "Okay, added a new reminder!~";
-      }
-    }); 
-  }
-} 
+        this.subscribeToPluginEvent(PluginEvent.BotShutdown, (data: EmptyEventArguments) => {
+            Plugin._runningChecks = false;
+            this.saveState();
+        });
+    }
+
+    /**
+     * @override
+     */
+    public getPluginSpecificCommands(): BotCommand[] {
+        const remindMeCommand = new BotCommand(["remindme"], "Have DankTimesBot remind you of something", this.remindMe.bind(this));
+        return [remindMeCommand];
+    }
+
+    private onBotStartup(eventArgs: EmptyEventArguments): void {
+        const data = this.loadDataFromFile<{ [chatId: number]: RemindMeStateCache }>(Plugin.REMIND_ME_DATA_FILE) ?? {};
+        this.remindMeData = data;
+        Plugin._runningChecks = true;
+
+        setTimeout(this.doReminderMeChecks.bind(this), 1 * 1000);
+    }
+
+    private remindMe(chat: Chat, user: User, msg: TelegramBot.Message, match: string): string {
+        let reply = "";
+
+        if (Plugin.qtyUnitPattern.test(msg.text)) {
+            const {qty, unit, reason} = Plugin.qtyUnitPattern.exec(msg.text).groups;
+            reply = this.setReminderForUserAfterQtyUnit(user, chat, msg, +qty, unit, reason);
+        } else if (Plugin.hourMinutePattern.test(msg.text)) {
+            const {hour, minute, reason} = Plugin.hourMinutePattern.exec(msg.text).groups;
+            reply = this.setReminderForUserAtHourAndMinute(user, chat, msg, +hour, +minute, reason);
+        } else {
+            reply = "⏰ Usage: /remindme [hh:mm] [reason] | /remindme [number] [seconds|minutes|hours|days] [reason]";
+        }
+
+        this.telegramBotClient.sendMessage(chat.id, reply, {reply_to_message_id: msg.message_id});
+        return "";
+    }
+
+    private setReminderForUserAfterQtyUnit(user: User, chat: Chat, message: TelegramBot.Message, qty: number, unit: string, reason: string) {
+        if (qty <= 0) {
+            return "⚠️ I can't remind you of something in the past";
+        }
+
+        const duration = {
+            "second": 1000,
+            "seconds": 1000,
+            "minute": 60000,
+            "minutes": 60000,
+            "hour": 3600000,
+            "hours": 3600000,
+            "day": 86400000,
+            "days": 86400000
+        };
+
+        const offset = -1 * (new Date()).getTimezoneOffset() * 60000;
+
+        const reminderDuration = duration[unit] * qty;
+        if (!reminderDuration) {
+            return "⚠️ I don't know this unit. Use second(s), minute(s), hour(s) or day(s)";
+        }
+
+        if (!this.remindMeData[chat.id]) {
+            this.remindMeData[chat.id] = {
+                chatId: chat.id,
+                reminders: []
+            };
+        }
+
+        const setDate = (new Date(new Date().valueOf() + +reminderDuration));
+        this.remindMeData[chat.id].reminders.push({
+            reminded: false,
+            reason: reason,
+            messageId: message.message_id,
+            remindUserAt: setDate,
+            user: user.id
+        });
+
+        const s = new Date(setDate.valueOf() + offset).toISOString().split("T");
+        const t = s[1].split("Z")[0];
+        return `⏰ Set a reminder for ${s[0]} on ${t}`;
+    }
+
+    private setReminderForUserAtHourAndMinute(user: User, chat: Chat, message: TelegramBot.Message, hour: number, minute: number, reason: string) {
+        if (hour < 0 || hour > 23) {
+            return "⚠️ Enter an hour between 0 and 23";
+        } else if (minute < 0 || minute > 59) {
+            return "⚠️ Enter a minute between 0 and 59";
+        }
+
+        const now = new Date();
+        const offset = (-1 * now.getTimezoneOffset()) * 60000;
+        const isTommorow = hour < now.getHours() || (hour === now.getHours() && minute <= now.getMinutes());
+        const setDate = new Date();
+        setDate.setDate(setDate.getDate() + (isTommorow ? 1:0));
+        setDate.setHours(+hour);
+        setDate.setMinutes(+minute);
+        const s = new Date(setDate.valueOf() + offset).toISOString().split("T");
+        const t = s[1].split("Z")[0];
+
+        if (!this.remindMeData[chat.id]) {
+            this.remindMeData[chat.id] = {
+                chatId: chat.id,
+                reminders: []
+            };
+        }
+
+        this.remindMeData[chat.id].reminders.push({
+            reminded: false,
+            reason: reason,
+            messageId: message.message_id,
+            remindUserAt: setDate,
+            user: user.id
+        });
+
+        return `⏰ Set a reminder for ${s[0]} on ${t}`;
+    }
+
+    private doReminderMeChecks() {
+        if (!Plugin._sCheckInProgress && Plugin._runningChecks) {
+            try {
+                Plugin._sCheckInProgress = true;
+                let save = false;
+                for (const c in this.remindMeData) {
+                    const now = new Date();
+                    const remindersToSend = this.remindMeData[+c].reminders.filter(r => !r.reminded && new Date(r.remindUserAt) < now);
+                    for (const r of remindersToSend) {
+
+                        const timeAgo = ((new Date().getTime() - new Date(r.remindUserAt).getTime()) / 60000);
+                        // Only send messages which aren't older than 15 minutes or so
+                        if (timeAgo <= 15) {
+                            this.telegramBotClient.sendMessage(+c, `⏰ Reminder: ${r.reason}`, {
+                                reply_to_message_id: r.messageId
+                            }).then();
+                        }
+                        r.reminded = true;
+                        save = true;
+                    }
+
+                    this.remindMeData[+c].reminders = this.remindMeData[+c].reminders.filter(r => !r.reminded);
+                }
+
+                if(save) {
+                    this.saveState();
+                }
+            } catch (e) {
+                console.log("err", e);
+            } finally {
+                Plugin._sCheckInProgress = false;
+            }
+        }
+
+        setTimeout(this.doReminderMeChecks.bind(this), 1000);
+    }
+
+    private saveState() {
+        this.saveDataToFile(Plugin.REMIND_ME_DATA_FILE, this.remindMeData);
+    }
+
+    private clearRemindersForChat(data: ChatResetEventArguments) {
+        if (this.remindMeData[+(data.chat.id)]) {
+            delete this.remindMeData[+(data.chat.id)];
+        }
+        this.saveState();
+    }
+}
